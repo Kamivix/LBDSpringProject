763136eedd91c1c4e11ff8e11943bf04572ebc30
kamil <kamil.partyka1998@gmail.com>
2022-07-05 10:36:58 +0200

    Project has been created

---
+++ src/main/java/com/example/lbdspringproject/LbdSpringProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.lbdspringproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LbdSpringProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LbdSpringProjectApplication.class, args);
+    }
+
+}
